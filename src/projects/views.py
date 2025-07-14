d86169df18c1f76fb77e5cd882a7892017cd34af
GliderGeek <GliderGeek@users.noreply.github.com>
2025-07-14 11:16:34 +0200

    fix duplication

--- src/projects/views.py
+++ src/projects/views.py
@@ -30,27 +30,6 @@ class RVOFormMixin:
             elif isinstance(widget, forms.SelectMultiple):
                 widget.attrs['class'] = widget.attrs.get('class', '') + ' utrecht-select utrecht-select--html-select utrecht-select--multiple'
 
-
-class RVOFormMixin:
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for name, field in self.fields.items():
-            widget = field.widget
-            if name == "colleagues":
-                # Forceer de juiste class voor Select2
-                widget.attrs['class'] = "js-colleague-select utrecht-select utrecht-select--html-select"
-            elif isinstance(widget, forms.DateInput):
-                widget.input_type = 'date'
-                widget.attrs['class'] = widget.attrs.get('class', '') + ' utrecht-textbox utrecht-textbox--html-input utrecht-textbox--sm'
-            elif isinstance(widget, forms.TextInput):
-                widget.attrs['class'] = widget.attrs.get('class', '') + ' utrecht-textbox utrecht-textbox--html-input'
-            elif isinstance(widget, forms.Textarea):
-                widget.attrs['class'] = widget.attrs.get('class', '') + ' utrecht-textarea utrecht-textarea--html-textarea'
-            elif isinstance(widget, forms.Select):
-                widget.attrs['class'] = widget.attrs.get('class', '') + ' utrecht-select utrecht-select--html-select'
-            elif isinstance(widget, forms.SelectMultiple):
-                widget.attrs['class'] = widget.attrs.get('class', '') + ' utrecht-select utrecht-select--html-select utrecht-select--multiple'
-
 class ProjectForm(RVOFormMixin, forms.ModelForm):
     class Meta:
         model = Project
