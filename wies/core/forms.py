da8e4e9355012b7d2e2661693527e497ff6acbfd
GliderGeek <GliderGeek@users.noreply.github.com>
2025-11-12 17:37:56 +0100

    add user form

---
+++ wies/core/forms.py
@@ -0,0 +1,45 @@
+from django import forms
+
+from .models import User, Brand
+
+
+class UserForm(forms.ModelForm):
+    """Form for creating and updating User instances"""
+
+    first_name = forms.CharField(
+        label='Voornaam',
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'utrecht-textbox utrecht-textbox--html-input rvo-input',
+        }),
+    )
+
+    last_name = forms.CharField(
+        label='Achternaam',
+        required=True,
+        widget=forms.TextInput(attrs={
+            'class': 'utrecht-textbox utrecht-textbox--html-input rvo-input',
+        }),
+    )
+
+    email = forms.EmailField(
+        label='Email',
+        required=True,
+        widget=forms.EmailInput(attrs={
+            'class': 'utrecht-textbox utrecht-textbox--html-input rvo-input',
+        }),
+    )
+
+    brand = forms.ModelChoiceField(
+        label='Merk',
+        queryset=Brand.objects.all(),
+        required=False,
+        empty_label='Geen merk',
+        widget=forms.Select(attrs={
+            'class': 'utrecht-select utrecht-select--html-select rvo-select',
+        }),
+    )
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'email', 'brand']
