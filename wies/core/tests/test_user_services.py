8499e56583bb732fd5829330dd60b27a4e937b90
GliderGeek <GliderGeek@users.noreply.github.com>
2026-01-05 14:58:16 +0100

    add tests on event creation

--- wies/core/tests/test_user_services.py
+++ wies/core/tests/test_user_services.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from wies.core.models import User
+from wies.core.models import User, Event
 from wies.core.errors import EmailNotAvailableError
 from wies.core.services.users import create_user, update_user
 
@@ -24,7 +24,13 @@ class CreateUserServiceTest(TestCase):
         self.assertIsNotNone(user.username)  # UUID should be generated
         self.assertTrue(User.objects.filter(email="newuser@example.com").exists())
 
-        # TODO: test that event is created
+        # Verify that event was created
+        event = Event.objects.filter(name='User.create', context__created_id=user.id).first()
+        self.assertIsNotNone(event)
+        self.assertEqual(event.user_id, 0)  # System-created (creator is None)
+        self.assertEqual(event.context['email'], 'newuser@example.com')
+        self.assertEqual(event.context['first_name'], 'New')
+        self.assertEqual(event.context['last_name'], 'User')
 
         user2 = create_user(
             user,
@@ -33,7 +39,13 @@ class CreateUserServiceTest(TestCase):
             email="newuser2@example.com",
         )
 
-        # TODO: verify that event is created and that user2 is logged
+        # Verify that event is created and creator user is logged
+        event2 = Event.objects.filter(name='User.create', context__created_id=user2.id).first()
+        self.assertIsNotNone(event2)
+        self.assertEqual(event2.user_id, user.id)  # Creator is user
+        self.assertEqual(event2.context['email'], 'newuser2@example.com')
+        self.assertEqual(event2.context['first_name'], 'New2')
+        self.assertEqual(event2.context['last_name'], 'User2')
 
 
     def test_create_user_duplicate_email(self):
@@ -87,7 +99,13 @@ class UpdateUserServiceTest(TestCase):
         self.assertEqual(updated_user.last_name, "NewName")
         self.assertEqual(updated_user.email, "updated@example.com")
 
-        # TODO: test if event has been created
+        # Verify that event was created
+        event = Event.objects.filter(name='User.update', context__updated_id=user.id).first()
+        self.assertIsNotNone(event)
+        self.assertEqual(event.user_id, 0)  # Updater is None (system)
+        self.assertEqual(event.context['email'], 'updated@example.com')
+        self.assertEqual(event.context['firstname'], 'Updated')
+        self.assertEqual(event.context['last_name'], 'NewName')
 
         # Verify changes were persisted
         user.refresh_from_db()
