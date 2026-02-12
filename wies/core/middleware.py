9c3c6e2f250c109ced574668a593980b7bb89d74
Ruben Rouwhof <ruben.rouwhof@rijksoverheid.nl>
2026-02-12 16:07:22 +0100

    Use DEV_EMAIL for auto-login instead of superuser

--- wies/core/middleware.py
+++ wies/core/middleware.py
@@ -1,4 +1,5 @@
 import logging
+import os
 
 from django.contrib.auth import get_user_model, login
 
@@ -8,14 +9,19 @@ User = get_user_model()
 
 
 class AutoLoginMiddleware:
-    """Auto-login as first superuser or staff user for local development without VPN/SSO."""
+    """Auto-login as developer user for local development without VPN/SSO."""
 
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         if not request.user.is_authenticated:
-            user = User.objects.filter(is_superuser=True).first() or User.objects.first()
+            dev_email = os.environ.get("DEV_EMAIL", "")
+            user = None
+            if dev_email:
+                user = User.objects.filter(email=dev_email).first()
+            if not user:
+                user = User.objects.filter(is_superuser=True).first() or User.objects.first()
             if user:
                 login(request, user, backend="django.contrib.auth.backends.ModelBackend")
                 logger.info("Auto-login: logged in as %s", user)
