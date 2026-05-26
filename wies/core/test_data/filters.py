e0c95eda57009a7e3229f18e3e3ef9f1fb27861e
r.djaspan <r.djaspan@dictu.nl>
2026-05-26 11:01:55 +0200

    filterlijsten

---
+++ wies/core/test_data/filters.py
@@ -0,0 +1,84 @@
+opdrachtgevers = [
+    {"Agentschappen*"},
+    {"Dienst Justitiële Inrichtingen*"},
+    {"Rijksinstituut voor*"},
+    {"Rijkswaterstaat*"},
+    {"Ministeries*"},
+    {"Ministerie van Algemene Zaken*"},
+    {"Ministerie van Financiën*"},
+    {"Directoraat-generaal*"}
+]
+
+rollen = [
+    {"AI Jurist"},
+    {"Backend development"},
+    {"Beleidsadviseur"},
+    {"Business Analist"},
+    {"CIO"},
+    {"Cybersecurity Specialist"},
+    {"Data Architect"},
+    {"Data engineer"},
+    {"DevOps Engineer"},
+    {"Frontend development"},
+    {"I-Adviseur"},
+    {"Information Manager"},
+    {"(Interim) Manager"},
+    {"Kwartiermaker"},
+    {"Organisatieadviseur"},
+    {"Privacy Officer"},
+    {"Procesbegeleider"},
+    {"Process Analyst"},
+    {"Product owner"},
+    {"Programmamanager"},
+    {"Project leader"},
+    {"Researcher"},
+    {"Scrum Master"},
+    {"Solution Architect"},
+    {"Test Manager"},
+    {"UX designer"},
+    {"Verandermanager"}
+]
+
+expertise = [
+    {"Agile, project- programma- en"},
+    {"AI"},
+    {"Architectuur en technologie"},
+    {"Cloud en platform technologie"},
+    {"ICT"},
+    {"Interimmanagement en advies"},
+    {"Kennis- en innovatiemanagement"},
+    {"Opleiding, training en"},
+    {"Proces- en ketenmanagement"},
+    {"Security en privacy"},
+    {"Software en data engineering"},
+    {"Strategie, beleid, governance"},
+    {"Verander- en"}
+]
+
+merk = [
+    {"Delta review"},
+    {"Gateway review"},
+    {"I-Interim Rijk"},
+    {"Innoveren met Impact"},
+    {"Intercoach"},
+    {"Leer en ontwikkel campus"},
+    {"Mindful Rijk"},
+    {"RADIO"},
+    {"Rijksconsultants"},
+    {"Rijks ICT Gilde"},
+    {"Rijks I-Traineeship"}
+]
+
+thema = [
+    {"Ambtelijk en digitaal"},
+    {"Artificiële intelligentie"},
+    {"Digitale weerbaarheid"},
+    {"Innovatieve en lerende"},
+    {"Netwerksamenwerking"}
+]
+
+loopt_af = [
+    {"Binnen 3 maanden"},
+    {"Binnen 6 maanden"},
+    {"Langer dan 6 maanden"}
+]
